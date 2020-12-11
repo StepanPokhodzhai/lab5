3f20d259fed9149b4aaf8fef638e6dda34f32788
StepanPokhodzhai <62850777+StepanPokhodzhai@users.noreply.github.com>
2020-12-11 10:25:40 +0200

    Add files via upload

---
+++ script/myjs.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        autoHeight:true,
+        items:1,
+        nav:true
+    });
+    $(function() {
+        $( "#tabs" ).tabs().addClass("ui-tabs-vertical");
+    });
+});
